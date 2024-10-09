579e40825909ba0cfe9d953c890e29a5a2b9eef0
JeonghyeHa <hajeonghye6786@gmail.com>
2024-10-10 08:46:39 +0900

    환경변수 3차 시도

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 import mysql.connector
 
 app = Flask(__name__)
-
-# 환경 변수 출력으로 확인
-from dotenv import load_dotenv
-import os
 
 # .env 파일에서 환경 변수를 로드
 load_dotenv()
